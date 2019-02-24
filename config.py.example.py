b8820ba14e06e6ed092307f84173b8d28b9a0c46
e36 <Tqbfjotld1>
2019-02-24 08:52:16 -0600

    quick fixes typos and syntax fixes

--- config.py.example.py
+++ config.py.example.py
@@ -1,32 +1,32 @@
-#APP CONFIG
+# APP CONFIG
 SECRET_KEY = "type a long random string"
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # set up the database engine information here
 database = {
-    'engine':'postgresql',
-    'hostname':'localhost',
-    'port':'5432',
-    'database':'test',
-    'user':'testuser',
-    'password':'password'
+    'engine': 'postgresql',
+    'hostname': 'localhost',
+    'port': '5432',
+    'database': 'test',
+    'user': 'testuser',
+    'password': 'password'
 }
 
 # postgresql://scott:tiger@localhost/mydatabase
 SQLALCHEMY_DATABASE_URI = database['engine'] + "://" + database['user'] + ":" + database['password'] + "@" + database['hostname'] + "/" + database['database']
 
 # the drop directory, where files will be picked up by the file processor job
-drop_directory='drop/'
+drop_directory = 'drop/'
 
 # the storage directory, where the documents will be saved to once they've been processed
-document_directory='static/documents/'
+document_directory = 'static/documents/'
 
 # thumbnail directory, where the document thumbnails will be saved
-thumbnail_directory='static/thumbnails/'
+thumbnail_directory = 'static/thumbnails/'
 
 # file processor logging file
-processor_log_file='processorlog.log'
+processor_log_file = 'processorlog.log'
 
 # temp folder
 temp_directory = 'temp/'
